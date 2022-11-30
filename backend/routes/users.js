8b7211f9b32665a52cc408133dadee2254926739
Damiaan <ddamian2057@gmail.com>
2022-11-30 18:51:23 +0100

    the beginning of work

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const app = express();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
 
 module.exports = router;
