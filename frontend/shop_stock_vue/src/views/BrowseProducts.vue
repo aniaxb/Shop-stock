70596c1c2054ab8cceb3de81b66305fea1e6f0bd
aniaxb <a.banachowicz11@gmail.com>
2022-11-30 14:34:26 +0100

    Created components and added Bootstrap with Popper.js

---
+++ frontend/shop_stock_vue/src/views/BrowseProducts.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+       <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light " id="mainNavbar">
+  <!-- Container wrapper -->
+  <div class="container">
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#"><img id="MDB-logo"
+        src="http://cdn.onlinewebfonts.com/svg/img_473618.png" alt="MDB Logo"
+        draggable="false" height="30" /></a>
+
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left links -->
+      <ul class="navbar-nav me-3">
+        <li class="nav-item">
+          <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
+            <RouterLink to="/" class="link">Home</RouterLink></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
+            <RouterLink to="/browse" class="link">Browse</RouterLink></a>
+        </li>
+      </ul>
+      <!-- Left links -->
+
+      <form class="d-flex align-items-center w-100 form-search">
+        <div class="input-group">
+          <button class="btn btn-light dropdown-toggle shadow-0" type="button" data-mdb-toggle="dropdown"
+            aria-expanded="false" style="padding-bottom: 0.4rem;">
+            All
+          </button>
+          <ul class="dropdown-menu dropdown dropdown-menu-dark fa-ul">
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-search"></i></span>All</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-film"></i></span>Titles</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-tv"></i></span>TV
+                Episodes</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-user-friends"></i></span>Celebs</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-building"></i></span>Companies</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-key"></i></span>Keywords</a>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-search-plus"></i></span>Advanced
+                search<i class="fas fa-chevron-right ps-2"></i></a>
+            </li>
+          </ul>
+          <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
+        </div>
+        <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
+      </form>
+
+      <ul class="navbar-nav ms-3">
+        <li class="nav-item me-3">
+          <a class="nav-link d-flex align-items-center" href="#!">ShoeStockPro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link d-flex align-items-center me-3" href="#!">
+            <i class="fas fa-bookmark pe-2"></i> Watchlist
+          </a>
+        </li>
+        <li class="nav-item" style="width: 65px;">
+          <a class="nav-link d-flex align-items-center" href="#!"><RouterLink to="/login" class="link">Login</RouterLink></a>
+        </li>
+      </ul>
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
+    </div>
+    <div>
+        <h1>Hello BrowsePage</h1>
+        
+        
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+#mainNavbar {
+    background-color: #757bc8;
+}
+
+body {
+    background-color: #e0c3fc;
+}
+
+.link {
+    text-decoration: none;
+    color: black;
+}
+</style>
