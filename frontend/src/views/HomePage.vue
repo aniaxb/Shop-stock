b8b62f791cf26073afe3ffcaddc70f8319785c7d
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2022-12-01 20:46:27 +0100

    Changes in the interface

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,98 +1,40 @@
 <template>
-    <div>
-       <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light " id="mainNavbar">
-  <!-- Container wrapper -->
-  <div class="container">
-    <!-- Navbar brand -->
-    <a class="navbar-brand" href="#"><img id="MDB-logo"
-        src="http://cdn.onlinewebfonts.com/svg/img_473618.png" alt="MDB Logo"
-        draggable="false" height="30" /></a>
-
-    <!-- Toggle button -->
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-      aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Left links -->
-      <ul class="navbar-nav me-3">
-        <li class="nav-item">
-          <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
+   <nav id="mainNavbar" class="navbar navbar-dark navbar-expand-lg px-2" >
+  <a class="navbar-brand" href="#">
+    <img src="http://cdn.onlinewebfonts.com/svg/img_473618.png" alt="">
+    ShoeStock</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <!-- <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> -->
+        <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
             <RouterLink to="/" class="link">Home</RouterLink></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
             <RouterLink to="/browse" class="link">Browse</RouterLink></a>
-        </li>
-      </ul>
-      <!-- Left links -->
-
-      <form class="d-flex align-items-center w-100 form-search">
-        <div class="input-group">
-          <button class="btn btn-light dropdown-toggle shadow-0" type="button" data-mdb-toggle="dropdown"
-            aria-expanded="false" style="padding-bottom: 0.4rem;">
-            All
-          </button>
-          <ul class="dropdown-menu dropdown dropdown-menu-dark fa-ul">
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-search"></i></span>All</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                    class="fas fa-film"></i></span>Titles</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-tv"></i></span>TV
-                Episodes</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                    class="fas fa-user-friends"></i></span>Celebs</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                    class="fas fa-building"></i></span>Companies</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                    class="fas fa-key"></i></span>Keywords</a>
-            </li>
-            <li>
-              <hr class="dropdown-divider" />
-            </li>
-            <li>
-              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                    class="fas fa-search-plus"></i></span>Advanced
-                search<i class="fas fa-chevron-right ps-2"></i></a>
-            </li>
-          </ul>
-          <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active d-flex align-items-center" aria-current="page" href="#">
+            <RouterLink to="/login" class="link">Login</RouterLink></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-        <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
-      </form>
-
-      <ul class="navbar-nav ms-3">
-       
-        <li class="nav-item">
-          <a class="nav-link d-flex align-items-center me-3" href="#!">
-            <i class="fas fa-bookmark pe-2"></i> Watchlist
-          </a>
-        </li>
-        <li class="nav-item" style="width: 65px;">
-          <a class="nav-link d-flex align-items-center" href="#!"><RouterLink to="/login" class="link">Sign In</RouterLink></a>
-        </li>
-      </ul>
-    </div>
-    <!-- Collapsible wrapper -->
+      </li>
+    </ul>
   </div>
-  <!-- Container wrapper -->
 </nav>
-<!-- Navbar -->
-    </div>
     <div>
         <h1>Hello HomePage</h1>
         
@@ -101,19 +43,26 @@
 </template>
 
 <script>
+export default {
+  
+created() {
+    document.body.style.backgroundColor = "#f5f5f5";
+  },
+}
 </script>
 
 <style scoped>
 #mainNavbar {
-    background-color: #757bc8;
+    background-color: #594236;
 }
 
-body {
-    background-color: #e0c3fc;
+#mainNavbar img {
+  max-width: 50px;
+  max-height: 50px;
 }
 
 .link {
     text-decoration: none;
-    color: black;
+    color: white;
 }
 </style>
