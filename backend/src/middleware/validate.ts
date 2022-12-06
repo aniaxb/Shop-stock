97c9da99e1da5822c2148cd63e9226d85d86647d
Damiaan <ddamian2057@gmail.com>
2022-12-06 20:54:35 +0100

    repair of jwt import and creation of a schema for data validation

---
+++ backend/src/middleware/validate.ts
@@ -0,0 +1,20 @@
+import {User} from "../model/user";
+import {Product} from "../model/product";
+import {Order} from "../model/order";
+import {Category} from "../model/category";
+
+export function validateUser(user: User) {
+
+}
+
+export function validateProduct(product: Product) {
+
+}
+
+export function validateOrder(order: Order) {
+
+}
+
+export function validateCategory(category: Category) {
+
+}
