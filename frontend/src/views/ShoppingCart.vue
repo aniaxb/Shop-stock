be8aa04a20cef4738668dd4b27b3f80994cc48b3
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2022-12-13 17:13:26 +0100

    Added form in cart

--- frontend/src/views/ShoppingCart.vue
+++ frontend/src/views/ShoppingCart.vue
@@ -1,7 +1,11 @@
 <template>
-  <div><h1>Shopping Cart</h1></div>
+  <div><h1 class="text-center mb-5">Shopping Cart</h1></div>
   <div>
-    <div v-for="cartItem in cartItems" :key="cartItem.id" class="row mt-2 pt-3">
+    <div
+      v-for="cartItem in cartItems"
+      :key="cartItem.id"
+      class="row mt-2 pt-3 px-0 mx-0"
+    >
       <div class="col-2"></div>
       <div class="col-md-3">
         <img
@@ -20,21 +24,44 @@
           <p class="mb-0 font-weight-bold" id="item-price">
             $ {{ cartItem.price }} per unit
           </p>
-          <!-- <p class="mb-0">Quantity:{{ cartItem.quantity }}</p> -->
+          <!-- <p class="mb-0">Weight:{{ cartItem.weight }}</p> -->
         </div>
         <p class="mb-0">
           Total:
           <span class="font-weight-bold">
             $ {{ cartItem.price }}
-            <!-- * cartItem.quantity -->
+            <!-- * cartItem.weight -->
           </span>
         </p>
       </div>
-      <div class="col-2"></div>
-      <div class="col-12"><hr /></div>
+      <div class="col-12 px-0 mx-0"><hr /></div>
     </div>
 
-    <div class="total-cost pt-2 text-right">
+    <div class="">
+      <form class="col-8 mx-auto">
+        <div class="row mb-3">
+          <div class="col-3">
+            <label class="form-label" for="exampleUsername">Username</label>
+            <input type="text" class="form-control" placeholder="username" />
+          </div>
+          <div class="col-5">
+            <label for="exampleInputEmail" class="form-label"
+              >Email address</label
+            >
+            <input type="email" class="form-control" placeholder="email" />
+          </div>
+        </div>
+        <div class="mb-3 col-8">
+          <label for="examplePhone" class="form-label">Phone number</label>
+          <input type="number" class="form-control" placeholder="123123123" />
+        </div>
+        <button type="submit" class="btn col-8" id="submitButton">
+          Send Order
+        </button>
+      </form>
+    </div>
+
+    <div class="total-cost py-5 px-5">
       <h5>Total : ${{ this.totalCost }}</h5>
     </div>
   </div>
@@ -46,38 +73,13 @@
 export default {
   data() {
     return {
-      // cartItems: [
-      //   {
-      //     id: 2,
-      //     name: "Yeezy Boost 350 V2 Zebra",
-      //     description:
-      //       'Debuted in 2017, the adidas Yeezy Boost 350 V2 Zebra is known as one of the most renowned colorways in the Yeezy line. It features a white and black marbled Primeknit upper with a white side-stripe and red "SPLY-350" text. At the base, a cushioned Boost sole provides comfort and support.',
-      //     imgUrl:
-      //       "https://images.stockx.com/360/adidas-Yeezy-Boost-350-V2-Zebra/Images/adidas-Yeezy-Boost-350-V2-Zebra/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1635342807&h=384&q=57",
-      //     price: "220.00",
-      //     weight: "0.99",
-      //     categories: [],
-      //   },
-      // ],
       cartItems: Object,
-      // cartedProducts: [],
-
       token: null,
       totalCost: 0,
     };
   },
 
-  methods: {
-    countTotalCost(cartItem) {
-      // totalCost += cartItem.price;
-    },
-    // listCartItems(cartedProducts) {
-    //   // this.cartItems = cartedItems;
-    //   this.cartItems = JSON.parse(JSON.stringify(cartedProducts));
-    //   // this.cartItems = Array.from(cartedProducts);
-    //   console.log(this.cartItems);
-    // },
-  },
+  methods: {},
 
   mounted() {
     this.token = localStorage.getItem("token");
@@ -94,10 +96,8 @@ export default {
         }
         // console.log(this.totalCost);
       }
-      // this.countTotalCost();
     }
     // console.log(this.cartItems[0][0]);
-
   },
 };
 </script>
@@ -108,5 +108,8 @@ img {
   max-width: 100%;
   max-height: 100%;
   mix-blend-mode: multiply;
+}
+#submitButton {
+  background: #48acf0;
 }
 </style>
